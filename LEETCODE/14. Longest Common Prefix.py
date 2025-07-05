1b4ff6556b3af771c4c016ae94419ff132d350e7
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-07-05 19:54:14 +0300

    more optimized version for longest common prefix.py

--- LEETCODE/14. Longest Common Prefix.py
+++ LEETCODE/14. Longest Common Prefix.py
@@ -6,8 +6,6 @@ class Solution:
         for val in strs:
             if len(val) < len(min_str):
                 min_str = val
-
-
         ans = []
         i = -1
         status = True
@@ -21,9 +19,27 @@ class Solution:
                  ans.append(ch)
             else:
                  break
-        
         print(''.join(ans))
     
 
 # strs = ["flower","flow","flight"]
 # longestCommonPrefix(strs)
+
+# This is more optimized version(in both time and memory)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = []
+        try:
+            i = -1
+            for ch in strs[0]:
+                i += 1
+                for val in strs:  # we used for-else block to avoid using a status(boolean variable), because it automatically checks if the break is used or not
+                    if val[i] != ch:
+                        break
+                else:
+                    ans.append(ch)
+                    continue
+                break
+            return ("".join(ans))
+        except IndexError:    # help us catch if the first element is not the minumum length element, by raising an exception when trying to access element in length of first string but not others
+            return ("".join(ans))
