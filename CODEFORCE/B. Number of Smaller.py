60a4a22197f7ed7a498c244ed14513ed5f0eb8a5
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-09-26 19:47:05 +0300

    leet code exercise

--- CODEFORCE/B. Number of Smaller.py
+++ CODEFORCE/B. Number of Smaller.py
@@ -2,12 +2,24 @@ n,m = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
 
-l, r = 0, 0
-while l < n and r < m:
-    while l < n and a[l] < b[r]:
-        l += 1
-    print(l,end=" ")
-    r += 1
-while r < m:
-    print(l,end=" ")
-    r += 1
+p1,count = 0,0
+ans = []
+for v in b:
+    while p1<len(a) and a[p1] < v:
+        count += 1
+        p1 += 1
+    print(count, end = " ")
+
+# n,m = map(int,input().split())
+# a = list(map(int,input().split()))
+# b = list(map(int,input().split()))
+
+# l, r = 0, 0
+# while l < n and r < m:
+#     while l < n and a[l] < b[r]:
+#         l += 1
+#     print(l,end=" ")
+#     r += 1
+# while r < m:
+#     print(l,end=" ")
+#     r += 1
--- LEETCODE/2491. Divide Players Into Teams of Equal Skill.py
+++ LEETCODE/2491. Divide Players Into Teams of Equal Skill.py
@@ -3,19 +3,16 @@ from typing import List
 
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-
         skill.sort()
-        left = 1
-        right = len(skill) - 2
-        eq_sum = skill[0] + skill[right+1]
-        chem = skill[0] * skill[right+1]
-        while left < right:
-            if (skill[left] + skill[right]) == eq_sum:
-                chem += skill[left] * skill[right]
-                left += 1
-                right -= 1
+        l,chem,r = 0,0,len(skill) - 1
+        sum  = skill[l] + skill[r]
+        while l<r:
+            if skill[l] + skill[r] == sum:
+                chem += skill[l] +skill[r]
             else:
                 return -1
+            l += 1
+            r -= 1
         return chem
 
 
