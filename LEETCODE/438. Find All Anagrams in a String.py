9b5f127a2b58006672e0d4b28ea01425b362dda5
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-10-02 09:07:59 +0300

    leetcode problem finding all anagrams of a string

--- LEETCODE/438. Find All Anagrams in a String.py
+++ LEETCODE/438. Find All Anagrams in a String.py
@@ -1,34 +1,52 @@
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#         k,n = len(p),len(s)
+#         if k > n:
+#             return []
+#         target = {v:0 for v in p}
+#         window = {v:0 for v in p}
+#         a,ans = [],[]
+#         for v in p:
+#             target[v] += 1
+#             a.append(v)
+# # =============================================
+#         for i in range(k):
+#             if s[i] in a:
+#                 window[s[i]] += 1
+#         status = True
+#         for v in a:
+#             if window[v] != target[v]:
+#                 status = False
+#         if status:
+#             ans.append(i-k+1)
+# # ==============================================
+#         for i in range(k,n):
+#             if s[i-k] in a:
+#                 window[s[i-k]] -= 1
+#             if s[i] in a:
+#                 window[s[i]] += 1
+#             status = True
+#             for v in a:
+#                 if window[v] != target[v]:
+#                     status = False
+#             if status:
+#                 ans.append(i-k+1)
+#         return ans
+
+# THIS IS THE EFFICIENT WAY
+
+from typing import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        k,n = len(p),len(s)
-        if k > n:
-            return []
-        target = {v:0 for v in p}
-        window = {v:0 for v in p}
-        a,ans = [],[]
-        for v in p:
-            target[v] += 1
-            a.append(v)
-# =============================================
-        for i in range(k):
-            if s[i] in a:
-                window[s[i]] += 1
-        status = True
-        for v in a:
-            if window[v] != target[v]:
-                status = False
-        if status:
-            ans.append(i-k+1)
-# ==============================================
-        for i in range(k,n):
-            if s[i-k] in a:
-                window[s[i-k]] -= 1
-            if s[i] in a:
-                window[s[i]] += 1
-            status = True
-            for v in a:
-                if window[v] != target[v]:
-                    status = False
-            if status:
-                ans.append(i-k+1)
-        return ans
+        result = []
+        target = Counter(p)
+        window = Counter(s[:len(p)-1])
+        
+        for i in range(len(p)-1,len(s)):
+            window[s[i]] += 1
+            if window == target:
+                result.append(i-len(p)+1)
+            window[s[i-len(p)+1]] -= 1
+            if window[s[i-len(p)+1]] == 0:
+                del window[s[i-len(p)+1]]
+        return result
