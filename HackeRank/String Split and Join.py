ec16632b0ce86e3fea81dc4a0868934dce67b644
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-03 16:33:25 +0300

    Refactor split_and_join function and clean up code

--- HackeRank/String Split and Join.py
+++ HackeRank/String Split and Join.py
@@ -1,13 +1,8 @@
-# def split_and_join(line):
-#     # write your code here
-#     splitted = line.split()
-#     return "-".join(splitted)
-# if __name__ == '__main__':
-#     line = input()
-#     result = split_and_join(line)
-#     print(result)
+def split_and_join(line):
+    return("-".join(line.split()))
+    # write your code here
 
-if -2:
-    print("yes")
-else:
-    print("np")
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
