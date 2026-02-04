f23bf7af1166e5133632106d1a74964f4f2d6c61
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2026-02-04 19:42:26 +0300

    added comments, better variable naming for leetcode problem remove-comments

--- LEETCODE/722-remove-comments/remove-comments.py
+++ LEETCODE/722-remove-comments/remove-comments.py
@@ -1,18 +1,16 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
     
-
-        stack = False
-        tempstatus = False
+        stack = False  # False => we haven't encountered /* still    ||||| True => we have get /* befor (but not still */  when we get */ we will change it to False )
+        tempstatus = False  # used to show their is a fragment of code to make them togather because they are separated by a multiline comment. e.g. int x = /*this is comment*/5 ;  now int x = 5 ;
         ans = []
         temp = []
         for line in source:
-
-            n = len(line)
-            lastidx = n
-            start = 0
-
+            
             i = 0
+            n = len(line)
+            startidx = 0
+            lastidx = n
             while i < n:
 
                 if not stack and line[i] == '/' and i < n - 1:
@@ -20,24 +18,22 @@ class Solution:
                         lastidx = i
                         break
                     elif line[i+1] == '*':
-                        if line[start:i]:
-                            temp.append(line[start:i])
-                            # temp = line[start:i]
+                        if line[startidx:i]:
+                            temp.append(line[startidx:i])
                             tempstatus = True
                         stack = True
                         i += 1
                 elif stack and line[i] == '*' and i < n-1:
                     if line[i+1] == '/':
-                            stack = False
-                            start = i+2
-                            i += 1
+                        stack = False
+                        startidx = i+2
+                        i += 1
                 i += 1
             if not stack and tempstatus:
-                # ans.append(temp + line[start:lastidx])
-                ans.append("".join(temp) + line[start:lastidx])
+                ans.append("".join(temp) + line[startidx:lastidx])
                 tempstatus = False
                 temp = []
-            elif not stack and line[start:lastidx]:
-                ans.append(line[start:lastidx])
+            elif not stack and line[startidx:lastidx]:
+                ans.append(line[startidx:lastidx])
 
         return ans
