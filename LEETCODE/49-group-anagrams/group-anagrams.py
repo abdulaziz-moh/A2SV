605436d3e759f0dd841166bc985e1f9487485749
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-09 13:14:32 +0300

    Time: 11 ms (83.94%) | Memory: 22 MB (53.24%) - LeetSync

--- LEETCODE/49-group-anagrams/group-anagrams.py
+++ LEETCODE/49-group-anagrams/group-anagrams.py
@@ -1,9 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        sortedstr = []
         dd = defaultdict(list)
         for v in strs:
-            dd[str(sorted(v))].append(v)
+            dd["".join(sorted(v))].append(v)
         
         return list(dd.values())
