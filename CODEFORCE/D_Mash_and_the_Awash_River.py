163dfab5d495cfa4f88aef6b6398f92f0a2861d1
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2026-02-09 17:21:12 +0300

    optimized D_Mash_and_the_Awash_River solution

--- CODEFORCE/D_Mash_and_the_Awash_River.py
+++ CODEFORCE/D_Mash_and_the_Awash_River.py
@@ -1,37 +1,60 @@
+# t = int(input())
+
+# for i in range(t):
+#     a = input()
+#     n = len(a)
+        
+#     count = [1]
+#     for i in range(1,n):
+        
+#         if a[i] == a[i-1]:
+#             if a[i] == '<' or a[i] == '>':
+#                 count[-1] += 1
+#             else:
+#                 count.append(-1)
+#                 break
+#         elif a[i] == '<' and a[i-1] == '>':
+#             count.append(-1)
+#             break
+#         elif a[i] == '>' and a[i-1] == '<':
+#             count.append(1)
+                    
+#         elif a[i] == '*' and a[i-1] == '>':
+#             count.append(-1)
+#             break
+#         elif a[i] == '*' and a[i-1] == '<':
+#             count[-1] += 1
+        
+#         elif a[i] == '<' and a[i-1] == '*':
+#             count.append(-1)
+#             break
+#         elif a[i] == '>' and a[i-1] == '*':
+#             count.append(2)
+              
+#     if count[-1] == -1:
+#         print("-1")
+#     else:  
+#         print(str(max(count)))   
+
+
+# from typing import Counter
+
 t = int(input())
 
 for i in range(t):
     a = input()
     n = len(a)
-        
-    count = [1]
-    for i in range(1,n):
-        
-        if a[i] == a[i-1]:
-            if a[i] == '<' or a[i] == '>':
-                count[-1] += 1
-            else:
-                count.append(-1)
-                break
-        elif a[i] == '<' and a[i-1] == '>':
-            count.append(-1)
+    
+    for i in range(n):
+        if i+1 < n and a[i] in '*>' and a[i+1] in '*<':
+            print(-1)
             break
-        elif a[i] == '>' and a[i-1] == '<':
-            count.append(1)
-                    
-        elif a[i] == '*' and a[i-1] == '>':
-            count.append(-1)
-            break
-        elif a[i] == '*' and a[i-1] == '<':
-            count[-1] += 1
+    else:
+        # count = Counter(a)
+        # mn = min(count['<'],count['>'])
         
-        elif a[i] == '<' and a[i-1] == '*':
-            count.append(-1)
-            break
-        elif a[i] == '>' and a[i-1] == '*':
-            count.append(2)
-              
-    if count[-1] == -1:
-        print("-1")
-    else:  
-        print(str(max(count)))   
+        mn = min(a.count('<'),a.count('>'))
+        print(n-mn)
+    
+    
+    
