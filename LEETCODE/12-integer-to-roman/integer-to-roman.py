59e8b13254a0aac52742cf73b9f3b845cced2115
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-09 16:36:34 +0300

    Time: 9 ms (21.75%) | Memory: 19.3 MB (40.17%) - LeetSync

--- LEETCODE/12-integer-to-roman/integer-to-roman.py
+++ LEETCODE/12-integer-to-roman/integer-to-roman.py
@@ -1,28 +1,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        n = len(str(num))
-        decim = [0]*n
-        for i in range(1,n+1):
-            decim[n-i] = num%10
-            num //= 10
 
-        hm = {1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
+        hm = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I' }
         ans = []
-        for i in range(1,n+1):
-            p = 10**(n-i)
-            if decim[i-1] == 4:
-                ans.append(hm[p])
-                ans.append(hm[p*5])
-            elif decim[i-1] == 9:
-                ans.append(hm[p])
-                ans.append(hm[p*10])
-            elif decim[i-1] == 1 or decim[i-1] == 5:
-                ans.append(hm[p*decim[i-1]])
-            else:
-                r = decim[i-1]//5
-                rem = decim[i-1]%5
-                if r: 
-                    ans.append(hm[p*5])
-                for _ in range(rem):
-                    ans.append(hm[p])
+        
+        for k,v in hm.items():
+            
+            while num >= k:
+                ans.append(v)
+                num -= k
+            if num == 0:
+                break
+
         return "".join(ans)         
