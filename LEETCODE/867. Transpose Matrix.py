1ea2f8285c38647dff087c64df26186b7cb44d6e
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-12 11:57:03 +0300

    Refactor transpose method for square and non-square matrices

--- LEETCODE/867. Transpose Matrix.py
+++ LEETCODE/867. Transpose Matrix.py
@@ -2,12 +2,15 @@ class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         row = len(matrix)
         col = len(matrix[0])
-        ans = [[0]*row for _ in range(col)]
-        for i in range(row):
-            for j in range(col):
-                ans[j][i] = matrix[i][j]
-        return ans
-    
-obj = Solution()
-matrix = [[1,2],[4,5],[7,8]]
-print(obj.transpose(matrix))
+        if row == col:
+            for i in range(1, row):
+                for j in range(i):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            return matrix
+        else:
+            new_mrx = [[0]*row for _ in range(col)]
+
+            for i in range(row):
+                for j in range(col):
+                    new_mrx[j][i] = matrix[i][j]
+            return new_mrx
