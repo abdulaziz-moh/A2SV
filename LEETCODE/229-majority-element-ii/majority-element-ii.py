5c94a8514e4185b44670ec314d638aaf099dab1f
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-12 16:59:15 +0300

    Time: 16 ms (6.37%) | Memory: 22.4 MB (39.12%) - LeetSync

--- LEETCODE/229-majority-element-ii/majority-element-ii.py
+++ LEETCODE/229-majority-element-ii/majority-element-ii.py
@@ -1,16 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         
-        t = len(nums)//3
-        count = {}
-        ans = set()
+        candid = {}
         for v in nums:
-            if v in count:
-                count[v] += 1
+            if len(candid) < 3:
+                if v not in candid:
+                    candid[v] = 1
+                else:
+                    candid[v] += 1
+            elif v in candid:
+                candid[v] += 1
             else:
-                count[v] = 1
-
-            if count[v] > t:
-                    ans.add(v)
-        return list(ans)
-            
+                st = False
+                x = -1
+                for k,val in candid.items():
+                    if val == 0:
+                        x = k
+                        x = k
+                        st = True
+                        break
+                else:
+                    for k in candid:
+                        candid[k] -= 1
+                if st:
+                    del candid[x]
+                    candid[v] = 1
+        ans = [] 
+        t = len(nums)//3
+        for k in candid:
+            if nums.count(k) > t:
+                ans.append(k)
+        return ans
