6fd79c985bdad5c05f622b1243b7d02900e6f081
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2026-02-13 11:56:23 +0300

    codeforce jaggedswpas solution

--- CODEFORCE/A_Jagged_Swaps.py
+++ CODEFORCE/A_Jagged_Swaps.py
@@ -3,24 +3,7 @@ for _ in range(t):
     n = int(input())
     a = list(map(int,input().split()))
     
-    i = 1
-    if a[0] != 1:
-        print("NO")
-        continue
-    
-    
-    
-    while i < n-1:
-        if a[i] == i+1:
-            i += 1
-            continue
-        elif i+1 == a[i+1]:
-            a[i], a[i+1] = a[i+1], a[i]
-            i += 1
-            continue
-        print("NO")
-        break
+    if min(a) == a[0]:
+        print('YES')
     else:
-        print("YES")
-    print(a)
-            
+        print('NO')
