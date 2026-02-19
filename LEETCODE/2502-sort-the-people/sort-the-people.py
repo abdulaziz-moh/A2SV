bea246ce59a095f1fc485d30f223d46d7fa1d597
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-02-19 15:57:01 +0300

    Time: 332 ms (12.56%) | Memory: 20.7 MB (7.09%) - LeetSync

--- LEETCODE/2502-sort-the-people/sort-the-people.py
+++ LEETCODE/2502-sort-the-people/sort-the-people.py
@@ -4,19 +4,22 @@ class Solution:
         hm = {}
         for i in range(n):
             hm[heights[i]] = names[i]
-        
+
         # for i in range(1,n):
-        #     for j in range(n-i):
-        #         if heights[j] < heights[j + 1]:
-        #             heights[j],  heights[j + 1] = heights[j + 1], heights[j]
+        #     for j in range(i,0,-1):
+        #         if heights[j] > heights[j-1]:
+        #            heights[j] , heights[j-1] = heights[j-1], heights[j] 
+        #         else:
+        #             break
         # return [hm[heights[i]] for i in range(n)]
 
-        for i in range(1,n):
-            for j in range(i,0,-1):
-                if heights[j] > heights[j-1]:
-                   heights[j] , heights[j-1] = heights[j-1], heights[j] 
-                else:
-                    break
-        return [hm[heights[i]] for i in range(n)]
-                
-        
+        mx,mn = max(heights), min(heights)
+        count = [0] * (mx-mn+1)
+        for v in heights:
+            count[v-mn] += 1
+        ans = []
+        for i in range(len(count)):
+            for j in range(count[i]):
+                ans.append(mn + i)
+        ans.reverse()
+        return [hm[ans[i]] for i in range(n)]
