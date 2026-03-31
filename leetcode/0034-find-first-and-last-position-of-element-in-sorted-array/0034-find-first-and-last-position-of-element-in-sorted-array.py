8a4e9edf14c1aecf5207b2046e656d22a668560b
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-03-31 10:33:45 -0400

    Add solution for Find First and Last Position of Element in Sorted Array

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,43 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        idx, left, right = -1, 0, len(nums)-1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        start = right + 1
-
-        idx, left, right = -1, 0, len(nums)-1
-        while left <= right:
-            mid = (left + right) // 2
-
-            if nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        end = left - 1
-        if start <= end:
-            return [start, end]
+        def bsearch(tar):
+            idx, left, right = -1, 0, len(nums)-1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < tar:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            start = right + 1
+            return start
+        a = bsearch(target)
+        b = bsearch(target+1)
+        b -= 1
+        if a <= b:
+            return [a,b]
         return [-1,-1]
+
+
+
+        # idx, left, right = -1, 0, len(nums)-1
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        # start = right + 1
+
+        # idx, left, right = -1, 0, len(nums)-1
+        # while left <= right:
+        #     mid = (left + right) // 2
+
+        #     if nums[mid] > target:
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
+        # end = left - 1
+        # if start <= end:
+        #     return [start, end]
+        # return [-1,-1]
