af8922d1fc8403853fc69e901ca378e699a0245e
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-04-01 03:33:16 -0400

    Add solution for Construct Binary Tree from Preorder and Postorder Traversal

--- leetcode/0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ leetcode/0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -13,27 +13,23 @@ class Solution:
         br = 0
         def rec(idx):
             nonlocal br
-            if len(visited) == n:
-                # print("entered here")
+            if len(visited) == n or idx in visited:
                 return
-            if idx in visited:
-                return
-            # while idx in visited:
-            #     idx += 1
+
             val = preorder[idx]
             node = TreeNode(val)
-            # print(val)
             visited.add(idx)
+
             if postidx[val] <= br:
                 while br < n and preidx[postorder[br]] in visited:
                     br += 1
-                # print("br: ", br)
                 return node
+
             node.left = rec(idx + 1)
-            # print(node)
             
             rightval = postorder[postidx[val] - 1]
             a = preidx[rightval]
+            
             node.right = rec(a)
             
             return node
