2555d1ff3a1e24d89023d9b2238a8aa41bb0c559
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-04-01 00:50:11 -0400

    Add solution for Capacity To Ship Packages Within D Days

---
+++ leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        def check_valid_capacity(num):
+            count = 0
+            sum = 0
+            for v in weights:
+                sum += v
+                status = True
+                if sum == num:
+                    count += 1
+                    sum = 0
+                elif sum > num:
+                    count += 1
+                    sum = v
+                    status = False
+
+                else:
+                    status = False
+            if not status:
+                count += 1
+            if count <= days:
+                return True
+            return False
+
+        left, right = max(weights), sum(weights)
+        while left <= right:
+            mid = (left + right) // 2
+            # print(left, " ", mid, " ", right)
+            if check_valid_capacity(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right + 1
+
+        
